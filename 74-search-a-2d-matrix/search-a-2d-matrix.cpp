bd3c5135397891d55e869a962e30ed5594ef6221
Rydhym Agarwal <agarwalrydhym@gmail.com>
2026-06-19 11:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.94%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,13 +4,28 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == target)
-                    return true;
+        int left = 0;
+        int right = m*n-1;    
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+
+            int row = mid/n;
+            int col = mid%n;
+
+            int val = matrix[row][col];
+
+            if(val == target){
+                return true;
+            }
+            else if(val<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
             }
         }
-
         return false;
+        
     }
 };
