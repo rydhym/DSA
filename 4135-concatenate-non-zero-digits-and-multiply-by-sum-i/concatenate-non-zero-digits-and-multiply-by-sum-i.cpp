e1ea0a3ed79a68329910a14fd4355acb43cea52b
Rydhym Agarwal <agarwalrydhym@gmail.com>
2026-07-07 11:31:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (42.26%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        string s = to_string(n);
+        long long x = 0;
+        int sum=0;
+        for(char c: s){
+            if(c!='0'){
+                int d = c-'0';
+                x = x*10+d;
+                sum+=d;
+            }
+        }
+        return x*sum;
+    }
+};
